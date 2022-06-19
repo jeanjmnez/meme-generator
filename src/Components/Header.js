787db57163367ea7e68867c9f1ddef2241e18c70
Jean Carlos Jimenez Dominguez <jeansoft@hotmail.com>
2022-06-19 09:37:00 -0400

    Adding the meme data array and fixing css for the search

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,10 @@
+import trollface from "../images/troll-face.png";
+
 const Header = (props) => {
   return (
     <header className="header">
-      <img src="./images/troll-face.png" className="header--image" />
-      <h2 className="header--title">Header</h2>
+      <img src={trollface} className="header--image" alt="troll face" />
+      <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Meme Generator</h4>
     </header>
   );
--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
+import memesData from "../memesData.js";
+
 const Meme = (props) => {
+  const [imageUrl, setImageUrl] = useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    var randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setImageUrl(url);
+    console.log(url);
+  }
   return (
     <main>
-      <form>
+      <div className="form">
         <input type="text" className="form--input" placeholder="Top Text" />
         <input type="text" className="form--input" placeholder="Bottom Text" />
-        <button className="form--button">Get a new meme image</button>
-      </form>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+        <img src={imageUrl} alt="no image" />
+      </div>
     </main>
   );
 };
