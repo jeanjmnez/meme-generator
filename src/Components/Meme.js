f7414434153840edeeb0583ed3f82ab7c9bf31a0
Jean Carlos Jimenez Dominguez <jeansoft@hotmail.com>
2022-06-29 20:34:53 -0400

    Addin api endpoint to load memes

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import memesData from "../memesData.js";
+import { useState, useEffect } from "react";
 
 const Meme = (props) => {
   const [meme, setMeme] = useState({
@@ -7,12 +6,17 @@ const Meme = (props) => {
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
-  const [allMemeImages, setAllMemeImages] = useState(memesData);
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
 
   function getMemeImage() {
-    const memesArray = allMemeImages.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
     setMeme((prevMeme) => ({
       ...prevMeme,
       randomImage: url,
@@ -26,7 +30,6 @@ const Meme = (props) => {
       [name]: value,
     }));
   }
-
   return (
     <main>
       <div className="form">
